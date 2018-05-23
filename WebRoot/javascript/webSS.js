3e0e5f87111cc7e7604c89d90e39345b21d60f9e
NianHao <1137136542@qq.com>
2018-05-23 17:42:45 +0800

    增加了一些按钮

--- WebRoot/javascript/webSS.js
+++ WebRoot/javascript/webSS.js
@@ -1,5 +1,5 @@
 var webSS=function(uId){
-	var webSocketHost='127.0.0.1/PGV2/'
+	var webSocketHost='192.168.0.117/PGV2/'
 	var serverEndPoint='webSocketServer/'
 	var uid=null;
 	var socket=null;
@@ -20,6 +20,28 @@ var webSS=function(uId){
 	var onError=function(error){
 		console.log(error);
 	}
+	/*
+	 * 心跳重连
+	 */
+	var heartCheck = {
+		    timeout: 60000,//60ms
+		    timeoutObj: null,
+		    serverTimeoutObj: null,
+		    reset: function(){
+		        clearTimeout(this.timeoutObj);
+		        clearTimeout(this.serverTimeoutObj);
+		　　　　 this.start();
+		    },
+		    start: function(){
+		        var self = this;
+		        this.timeoutObj = setTimeout(function(){
+		            window.socket.send("HeartBeat");
+		            //self.serverTimeoutObj = setTimeout(function(){
+		              //  ws.close();//如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
+		            //}, self.timeout)
+		        }, this.timeout)
+		    },
+		}
 	uid=uId;
 	socket=new WebSocket("wss://"+webSocketHost+serverEndPoint+uid);
 	socket.onopen=onOpen;
--- src/com/PGV2/websocket/WebSocketServer.java
+++ src/com/PGV2/websocket/WebSocketServer.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
@@ -41,7 +43,14 @@ public class WebSocketServer {
 		uid_session.remove(uid);
 		System.out.println(uid+"ÒÑ¾­±»É¾³ý");
 	}
-	
+	/*@OnMessage
+	public void onMessage(){
+		
+	}
+	@OnError
+	public void onError(){
+		
+	}*/
 	public static void transmitMessage(String toUid,String message){
 		
 		Session toSession=uid_session.get(toUid);
